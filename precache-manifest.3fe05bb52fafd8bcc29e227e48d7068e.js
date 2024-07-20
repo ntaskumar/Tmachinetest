23f9c9db5613a09bbdf5845f0a473149a2090997
ntaskumar <145751168+ntaskumar@users.noreply.github.com>
2024-07-20 08:20:25 +0530

    Add files via upload

---
+++ precache-manifest.3fe05bb52fafd8bcc29e227e48d7068e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a51d9ab9a64c564842cc93c923cfcd",
+    "url": "/Tmachinetest/index.html"
+  },
+  {
+    "revision": "3f0ffbada354bafa9670",
+    "url": "/Tmachinetest/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "392db98f26f92cfb2bb8",
+    "url": "/Tmachinetest/static/css/main.76e3cda8.chunk.css"
+  },
+  {
+    "revision": "3f0ffbada354bafa9670",
+    "url": "/Tmachinetest/static/js/2.fc0afb38.chunk.js"
+  },
+  {
+    "revision": "392db98f26f92cfb2bb8",
+    "url": "/Tmachinetest/static/js/main.3227a410.chunk.js"
+  },
+  {
+    "revision": "7db5abe76120ca626575",
+    "url": "/Tmachinetest/static/js/runtime~main.a733ed5e.js"
+  }
+]);
